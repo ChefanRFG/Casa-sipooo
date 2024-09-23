2eb72c9f9f1241a189bfff3e2553b65dfef2fa69
ChefanRFG <158303850+ChefanRFG@users.noreply.github.com>
2024-09-23 11:31:12 -0600

    Add files via upload

---
+++ Prueba.py
@@ -0,0 +1,5 @@
+Valor = 0
+print("Bienvenido al conversor de grados")
+val = float(input("1.Fahrenheit a grados centígrados o 2.grados centígrados a Fahrenheit -Introducir solo el numero de la opcion-"))
+if val == 1:
+    
